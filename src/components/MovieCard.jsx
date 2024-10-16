3001354c367d645249f092599f511ab76d0f74e2
Talaku Getahun <getahuntalaku@gmail.com>
2024-10-16 22:05:04 +0300

    Logic for MovieCard is defined but not tested

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function MovieCard() {
+function MovieCard({movie}) {
   return (
-    <div>MovieCard</div>
+    <div className='p-4'>
+        <div>
+            <Link to={`/movie/%{movie.id}`}>
+               <img
+                  className='' 
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                  alt={movie.title}
+            />
+            <div>
+                <h3>{movie.title}</h3>
+                <button className='mt-2 '>
+                    Add to Favorites 
+                </button>
+            </div>
+            </Link>
+        </div>
+    </div>
   )
 }
 
