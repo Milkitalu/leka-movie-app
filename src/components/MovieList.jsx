2f7f56a9328b4b9217b9ca9b0dd7a5ddecb88e8e
Talaku Getahun <getahuntalaku@gmail.com>
2024-10-16 21:53:53 +0300

    Logic for MovieLiST component is defined

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import useFetch from '../customHooks/useFetch';
+import MovieCard from './MovieCard';
+import Pagination from './Pagination';
+
+function MovieList({category}) {
+
+    const API_KEY = '';
+    const [currentPage, setCurrentPage] = useState(1); //sets initial page number to one.
+
+    // destructure result of data fetching 
+    const { data, loading, error } = useFetch(`https://api.themoviedb.org/3/movie/${category}?api_key=${API_KEY}&page=${currentPage}`);
+
+    if(loading) return <p> Loading....</p> ;
+    if(error) return <p>Movie Fetching Eroor </p>;
+
+    const totalPages = data.total_pages ;  // to hold data of total pages of movie lists.
+  return (
+    <div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {data.results.map(movie =>(
+                <MovieCard key = {movie.id} movie={movie} />
+            ))}
+        </div>
+        <Pagination totalPages={totalPages} currentPage={currentPage} inPageChange={setCurrentPage} />
+    </div>
+  );
+};
+
+export default MovieList
